b287b213984c5ae18969dfcdbc17ef4b695ac246
unknown <wangecilucy1234@gmail.com>
2023-12-04 09:09:22 +0300

    second

--- app/models.py
+++ app/models.py
@@ -1,7 +1,48 @@
 from django.db import models
-
+from django.utils import timezone
 # models.py
+
 from django.db import models
+class Employee(models.Model):
+    eid = models.CharField(max_length=20)
+    ename = models.CharField(max_length=100)
+    eemail = models.EmailField()
+    econtact = models.CharField(max_length=15)
+
+    def __str__(self):
+        return "%s " %(self.ename)
+    class Meta:
+        db_table = "employee"
+class Appointment(models.Model):
+    parent_name = models.CharField(max_length=255)
+    child_name = models.CharField(max_length=50)
+    appointment_date = models.DateField()
+    appointment_type = models.CharField(max_length=50, choices=[
+        ('home_babysitter', 'Home Babysitter'),
+        ('home_tutor', 'Home Tutor'),
+        ('school_babysitter', 'School Babysitter'),
+        ('school_tutor', 'School Tutor'),
+        ('home_playbuddy', 'Home Playbuddy'),
+        ('course_program', 'Course/Program'),
+       ],
+        default='home_babysitter'  # Choose the default value you prefer
+    )
+    duration = models.PositiveIntegerField()
+    payment_option = models.CharField(max_length=20, choices=[
+        ('credit_card', 'Credit Card'),
+        ('paypal', 'PayPal'),
+        ('cash', 'Cash'),
+    ],
+                                      default='credit_card'
+                                      )
+    amount_to_pay = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.parent_name} - {self.appointment_type}"
+
+
+
+
 
 class Child(models.Model):
     name = models.CharField(max_length=100)
@@ -32,7 +73,7 @@ class Hero(models.Model):
 
 
 
-class AboutSection(models.Model):
+class About(models.Model):
     video_url = models.URLField()
     title = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
@@ -105,14 +146,10 @@ class Testimonial(models.Model):
     def __str__(self):
         return self.name
 
+class Student(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Student Name")
+    email = models.EmailField(max_length=277, verbose_name="Student Email")
 
-class Employee(models.Model):
-    eid = models.PositiveIntegerField (max_length=8)
-    ename = models.CharField(max_length=100)
-    eemail = models.EmailField()
-    econtact = models.CharField(max_length=15)
 
     def __str__(self):
-        return "%s " %(self.ename)
-    class Meta:
-        db_table = "employee"
+        return str(self.id)
